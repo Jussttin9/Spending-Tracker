0bef8b5d40bb5632af0596ca2738c832de169ff7
jtran-9 <jr.tran79@gmail.com>
2024-09-04 17:41:29 -0700

    removed views

--- spending-tracker/server/app.js
+++ spending-tracker/server/app.js
@@ -10,19 +10,9 @@ const cors = require('cors');
 var usersRouter = require('./routes/users');
 var itemsRouter = require('./routes/items');
 
-const allowedOrigins = ['http://localhost:3000', 'https://in-the-bank.vercel.app'];
-
 var app = express();
 dotenv.config();
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
